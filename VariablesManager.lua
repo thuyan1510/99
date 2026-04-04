1890b3acc708570feb76771dbc2dd2d3b0bc94ab
thuyan1510 <thuyanai1510@gmail.com>
2026-04-04 14:11:09 +0700

    Create VariablesManager.lua

---
+++ VariablesManager.lua
@@ -0,0 +1,127 @@
+local VariablesManager = {}
+VariablesManager.__index = VariablesManager
+
+local getType = (typeof or type)
+local t_insert = table.insert
+local t_remove = table.remove
+local next = next
+
+function VariablesManager.new()
+    return setmetatable({
+        _v = {},
+        _t = {},
+        _c = {},
+        _ro = {}
+    }, VariablesManager)
+end
+
+function VariablesManagerAdd(name, value, expectedType, isReadOnly)
+    if self._v[name] ~= nil then return false, Variable already exists end
+    self._v[name] = value
+    self._t[name] = expectedType or getType(value)
+    if isReadOnly then self._ro[name] = true end
+    return true
+end
+
+function VariablesManagerSet(name, value)
+    local varType = self._t[name]
+    if not varType then return false, Variable not found end
+    if self._ro[name] then return false, Variable is read-only end
+
+    if getType(value) ~= varType then
+        return false, Type mismatch Expected  .. varType .. , got  .. getType(value)
+    end
+
+    local oldVal = self._v[name]
+    if oldVal ~= value then
+        self._v[name] = value
+        local cb = self._c[name]
+        if cb then cb(value, oldVal) end
+    end
+    return true
+end
+
+function VariablesManagerGet(name)
+    return self._v[name]
+end
+
+function VariablesManagerOnChange(name, callback)
+    if self._v[name] ~= nil then
+        self._c[name] = callback
+        return true
+    end
+    return false
+end
+
+function VariablesManagerIncrement(name, amount)
+    if self._t[name] == number and not self._ro[name] then
+        local current = self._v[name] or 0
+        return selfSet(name, current + (amount or 1))
+    end
+    return false, Variable is not a number or is read-only
+end
+
+function VariablesManagerTableInsert(name, value)
+    local tbl = self._v[name]
+    if self._t[name] ~= table or self._ro[name] then return false end
+    t_insert(tbl, value)
+    local cb = self._c[name]
+    if cb then cb(tbl, tbl) end
+    return true
+end
+
+function VariablesManagerTableRemove(name, indexOrValue)
+    local tbl = self._v[name]
+    if self._t[name] ~= table or self._ro[name] then return false end
+    if type(indexOrValue) == number then
+        t_remove(tbl, indexOrValue)
+    else
+        for i, v in ipairs(tbl) do
+            if v == indexOrValue then
+                t_remove(tbl, i)
+                break
+            end
+        end
+    end
+    local cb = self._c[name]
+    if cb then cb(tbl, tbl) end
+    return true
+end
+
+function VariablesManagerTableSet(name, key, value)
+    local tbl = self._v[name]
+    if self._t[name] ~= table or self._ro[name] then return false end
+    tbl[key] = value
+    local cb = self._c[name]
+    if cb then cb(tbl, tbl) end
+    return true
+end
+
+function VariablesManagerTableClear(name)
+    local tbl = self._v[name]
+    if self._t[name] ~= table or self._ro[name] then return false end
+    for k in next, tbl do tbl[k] = nil end
+    local cb = self._c[name]
+    if cb then cb(tbl, tbl) end
+    return true
+end
+
+function VariablesManagerBatchSet(data)
+    for name, value in next, data do
+        selfSet(name, value)
+    end
+end
+
+function VariablesManagerRemove(name)
+    self._v[name] = nil
+    self._t[name] = nil
+    self._c[name] = nil
+    self._ro[name] = nil
+end
+
+function VariablesManagerExport()
+    local dump = {}
+    for k, v in next, self._v do dump[k] = v end
+    return dump
+end
+return VariablesManager
