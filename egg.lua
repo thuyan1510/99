f225190ac25c675e4795bc24ad6a7f1228adf228
thuyan1510 <thuyanai1510@gmail.com>
2026-04-22 22:32:28 +0700

    Update egg.lua

--- egg.lua
+++ egg.lua
@@ -81,7 +81,13 @@ pcall(function()
         end
     end)
 end)
-
+local VirtualInputManager = game:GetService("VirtualInputManager")
+while task.wait() do
+        VirtualInputManager:SendKeyEvent(true, "Space", false, game)
+        task.wait(.2)
+        VirtualInputManager:SendKeyEvent(false, "Space", false, game)
+        task.wait(300)
+end
 -- ==========================================
 -- 🚀 EXTREME OPTIMIZE (GIẢM LAG)
 -- ==========================================
